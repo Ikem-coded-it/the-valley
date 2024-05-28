cfe63e7497f86ed1595fa0bc840dcf6c0b6c4df4
Ikemefuna Onubogu <ikemefunaonubogu55@gmail.com>
2024-05-28 15:44:26 +0100

    bout to push

--- src/template/SingleCommunities/sidebar.tsx
+++ src/template/SingleCommunities/sidebar.tsx
@@ -50,37 +50,6 @@ export default function SingleCommunitiesSideBar({
           />
         </SectionContainer>
       </div>{" "}
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-      <div className="mt-4">
-        <SectionContainer
-          title="Topics 📌 "
-          link={{
-            text: "See all",
-            path: "#",
-          }}
-        >
-          <TopicText
-            title="Agriculture & Biotech"
-            created={"1 hour"}
-            numOfPost={200}
-          />{" "}
-          <TopicText
-            title="Crypto & blockchain"
-            created={"1 hour"}
-            numOfPost={200}
-          />{" "}
-          <TopicText
-            title="Energy & climate"
-            created={"1 hour"}
-            numOfPost={200}
-          />
-        </SectionContainer>
-      </div>
->>>>>>> d9c637f (fix communities styling isuue)
-=======
->>>>>>> a67312d (style bug fix)
     </div>
   );
 }
